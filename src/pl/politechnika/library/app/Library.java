9b53055604b27f40535c9c502b14215b9fe7ba2a
Grzegorz Listwan <listwan94@gmail.com>
2023-04-16 10:46:26 +0200

    Dodano pakiet io oraz klasę DataReader odpowiedzialną za pobieranie danych od użytkownika

--- src/pl/politechnika/library/app/Library.java
+++ src/pl/politechnika/library/app/Library.java
@@ -1,19 +1,22 @@
 package pl.politechnika.library.app;
 
+import pl.politechnika.library.io.DataReader;
 import pl.politechnika.library.model.Book;
+
+import java.util.Scanner;
 
 class Library {
     public static void main(String[] args){
         final String appName= "Biblioteka v0.2";
         Book[] books = new Book[1000];
-       books[0] = new Book("W pustyni i puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg","88996456+465646");
-        books[1] = new Book("Java. Efektywne programowanie", "Nieznany", 2009, 352, "Helion","88945454456+465646");
-        books[2] = new Book("Dziady", "Adam Mickiewicz", 2000, 203, "Sowa");
-
+        DataReader dataReader = new DataReader(); // tworzymy obiekt klasy DataReader odpowiedzialnej za pobieranie danych od użytkownika
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.close();
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece");
         books[0].printInfo();
         books[1].printInfo();
-        books[2].printInfo();
+
     }
 }
---
+++ src/pl/politechnika/library/io/DataReader.java
@@ -0,0 +1,29 @@
+package pl.politechnika.library.io;
+
+import pl.politechnika.library.model.Book;
+
+import java.util.Scanner;
+
+public  class DataReader {
+     private Scanner sc = new Scanner(System.in); // prywatne pole obiektu Scanner do wczytywania danych z klawiatury
+      public Book readAndCreateBook(){ // metoda odpowiedzialna za pobieranie danych z klawiatury
+         System.out.println("Tytuł: ");
+         String title = sc.nextLine();
+         System.out.println("Autor: ");
+         String author = sc.nextLine();
+         System.out.println("Wydawnictwo: ");
+         String publisher = sc.nextLine();
+         System.out.println("ISBN: ");
+         String isbn = sc.nextLine();
+         System.out.println("Rok wydania: ");
+         int relaseDate = sc.nextInt();
+         sc.nextLine(); // pobieramy znak entera aby nie przeskoczyło o jedną linię i nie zgubic danych
+         System.out.println("Liczba stron: ");
+         int pages = sc.nextInt();
+         sc.nextLine();
+         return new Book(title,author,relaseDate,pages,publisher,isbn); // zwaracamy obiekt typu Book
+     }
+     public  void  close(){ // metoda zamykająca strumień skanera
+         sc.close();
+     }
+}
